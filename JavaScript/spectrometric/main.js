18b55318d30e195de41466d3779a610134e9f77c
MKolaksazov <49453937+MKolaksazov@users.noreply.github.com>
2024-06-01 04:03:40 -0700

    Add files via upload

---
+++ JavaScript/spectrometric/main.js
@@ -0,0 +1,231 @@
+
+function addVariant() {
+
+    var newRow = document.createElement('div');
+    var samples = document.getElementById('nSamples').value;
+    var idName = 'row_' + classID;
+
+    cont.appendChild(newRow);
+    newRow.classList.add('input-group', 'mb-3', 'row', 'g-3', "form-horizontal");
+    newRow.setAttribute('id', idName);
+
+    var label = document.createElement('label');
+    label.classList.add('input-group-text', 'col-sm');
+    label.innerHTML = document.getElementById('name').value;
+    labels.push(document.getElementById('name').value);
+    newRow.appendChild(label);
+    var dilution = document.createElement('label');
+    dilution.setAttribute("id",`dilute`);
+    dilution.classList.add('input-group-text', 'col-sm');
+    dilution.innerHTML = document.getElementById('dilution').value;
+    newRow.appendChild(dilution);
+    var method = document.createElement('label');
+    method.classList.add('input-group-text', 'col-sm');
+    method.innerHTML = document.getElementById('method').value;
+    newRow.appendChild(method);
+
+    for (i = 0; i < samples; i++) {
+            var cell = document.createElement('input');
+            cell.setAttribute("id",`input_${i}`);
+            cell.classList.add('form-control', 'col-sm', 'req');
+            cell.setAttribute("type", "number");
+            newRow.appendChild(cell);
+        }
+
+    var del = document.createElement('button');
+    del.classList.add('form-control', 'col-sm', 'btn', 'btn-danger', 'btn-sm', 'delete');
+    del.innerHTML = "remove";
+    del.setAttribute('id', idName + i);
+    del.setAttribute('onclick', 'removeRow("'+idName+'");');
+    newRow.appendChild(del);
+
+    classID += 1;
+}
+
+function populateOptions(id, res) {
+    var list = document.getElementById(id);
+    res.forEach((item) => {
+        var option = document.createElement('option');
+        option.value = item[0];
+        list.appendChild(option);
+    });
+}
+
+function removeRow(idRow) {
+  var currentRow = document.getElementById(idRow);
+  currentRow.remove();
+}
+
+function drawCalibration() {
+  //var methodIndex = checkIndex();
+  //setValues(methods[methodIndex][1].a, methods[methodIndex][1].b, methods[methodIndex][1].epsylon);
+
+  calculate();
+  drawLine();
+}
+
+function barGraph() {
+  //calculate();
+  drawBar(calculate());
+}
+
+function calculate() {
+  for (var i = 0; i < cont.children.length; i++) {
+    var inputs = cont.children[i].getElementsByTagName('input'); var row = [];
+    var methodIndex = checkIndex(cont.children[i].children[2].innerHTML);
+
+    for (var j = 0; j < inputs.length; j++) {
+      row.push(parseFloat(inputs[j].value));
+    }
+
+    dilutions.push([methodIndex,
+      parseFloat(cont.children[i].children[1].innerHTML), average(row)]);
+    calibration.push(row);
+  }
+
+  var calculated = [[],[],[]];
+
+  dilutions.forEach((x, i) => {
+    calculated[dilutions[i][0]].push(
+      (dilutions[i][2] + methods[dilutions[i][0]][1].b)
+      / methods[dilutions[i][0]][1].a * dilutions[i][1]);
+  });
+
+  console.log(calibration, dilutions, calculated);
+  return calculated;
+}
+
+function drawBar(calculated) {
+
+  var data = {
+      labels: labels,
+      datasets: [
+          {
+              label: "PM",
+              backgroundColor: "blue",
+              data: calculated[0]
+          },
+          {
+              label: "FRAP",
+              backgroundColor: "red",
+              data: calculated[1]
+          },
+          {
+              label: "Folin",
+              backgroundColor: "green",
+              data: calculated[2]
+          }
+      ]
+  };
+
+  var myBarChart = new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: {
+          barValueSpacing: 20,
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      min: 0,
+                  }
+              }]
+          }
+      }
+  });
+}
+
+function setValues(a, b, epsylon) {
+  document.getElementById("a").value = a;
+  document.getElementById("b").value = b;
+  document.getElementById("epsylon").value = epsylon;
+}
+
+function arr2obj(arr) {
+    let obj = [{}];
+      for (var n=0; n<arr.length; n++) {
+        let x = arr[n][0];
+        let y = arr[n][1];
+        obj.push({x:x, y:y});
+      }
+    return obj;
+}
+
+function calculateRegression() {
+    var regression = [];
+    var a = [];
+    var b = [];
+    for (i = 0; i<dilutions.length-1; i++) {
+      a.push((dilutions[i+1][1] - dilutions[i][1])
+        / (dilutions[i+1][0] - dilutions[i][0]));
+      b.push(dilutions[i][1] - (a[i] * dilutions[i][0]));
+    }
+
+    for (j = 0; j<dilutions.length; j++) {
+      yeval = a[j] * dilutions[j][0] + b[j];
+      regression.push({ x: dilutions[j][0], y: yeval});
+    }
+
+    return [average(a), average(b), regression];
+}
+
+
+function drawLine() {
+
+  if (dilutions.length === 0) { alert('Error! Unavailable data!'); return; }
+  //else { loopData(dilutions); }
+
+  var [a, b, regression] = calculateRegression();
+
+  setValues(a, b, a*1000);
+  /*
+  console.log(a, b, regression);
+  var json = {
+  "PM": {
+    "a": a,
+    "b": b
+  },
+  "FRAP": {
+    "a": a,
+    "b": b
+  },
+  "Folin": {
+    "a": a,
+    "b": b
+  }
+};
+  (json) => {
+    fs.writeFile(targetUrl, JSON.stringify(json), (err) => {
+        if (err) {
+            throw new Error('Something went wrong.')
+        }
+        console.log('JSON written to file. Contents:');
+        console.log(fs.readFileSync('test.json', 'utf-8'))
+    })
+}
+*/
+
+  var myChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+      datasets: [{
+        label: 'data',
+        data: arr2obj(dilutions),
+        borderColor: 'black',
+      },{
+        label: 'regression line',
+        data: regression,
+        borderColor: 'blue',
+        backgroundColor: 'transparent',
+        type: 'line'
+      }],
+    },
+    options: {}
+  });
+
+}
+
+function checkIndex(method) {
+    var indexElem = 0;
+    methods.forEach((x, i) => { if (method == x[0]) { indexElem = i; } });
+    return indexElem;
+}
---
+++ JavaScript/spectrometric/variables.js
@@ -0,0 +1,44 @@
+const targetUrl = 'https://mkolaksazov.github.io/Telerik-Academy/JavaScript/spectrometric/variables.json';
+
+fetch(targetUrl)
+    .then(response => response.json())
+    .then(data => {
+        for(var i in data) { methods.push([i, data[i]]); }
+        populateOptions('methodsList', methods);
+    });
+
+// ===========================VARIABLES==================================
+
+var cont = document.getElementById("cont");
+const average = array => array.reduce((a, b) => a + b) / array.length;
+var calibration = [];
+var dilutions = [];
+var methods = [];
+var labels = [];
+//var calculated = [];
+
+var speedCanvas = document.getElementById("myChart");
+var ctx = document.getElementById("myChart").getContext("2d");
+document.getElementById('myChart').style.height = '500px';
+
+Chart.defaults.global.defaultFontFamily = "Lato, sans-serif";
+Chart.defaults.global.defaultFontSize = 16;
+
+var classID = 0;
+
+// ===========================SHA256IMPLEMENTATION=======================
+
+async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder().encode(message);
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
