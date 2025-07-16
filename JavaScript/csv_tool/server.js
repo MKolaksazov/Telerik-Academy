2afe77684b81e2d916639f0f1f7ef51488461729
MKolaksazov <49453937+MKolaksazov@users.noreply.github.com>
2025-07-16 20:01:08 +0300

    Update server.js

--- JavaScript/csv_tool/server.js
+++ JavaScript/csv_tool/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3030;
 
 // Serve static files
 app.use(express.static('public'));
